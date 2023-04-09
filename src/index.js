fa313e1c278909c347cecb0f59a944c6b0d5c1ce
Matthew-uk <esaduviedede@gmail.com>
2023-04-09 18:31:56 +0100

    FIRST COMMIT

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const App = () => {
+  return <h1>This is the page!!!</h1>;
+};
+root.render(<App />);
